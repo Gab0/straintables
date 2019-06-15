e4cd7bcb18d26195bb581630daee85492cbd78ba
Gabriel Araujo <gabriel_scf@hotmail.com>
2019-06-15 19:46:14 -0300

    0.88

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ base_folder = os.path.dirname(os.path.realpath(__file__))
 requirements = list(open(os.path.join(base_folder, "requirements.txt")).readlines())
 setup(
     name='linkageMapper',
-    version='0.87',
+    version='0.88',
     description='Genomic similarities per region',
     author='Gabriel Araujo',
     author_email='gabriel_scf@hotmail.com',
