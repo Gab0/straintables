9be2a32606d427e9ff3cf698149df2ea5aec47da
Gabriel Araujo <gabriel_scf@hotmail.com>
2019-08-07 12:16:29 -0300

    new module

--- straintables/__init__.py
+++ straintables/__init__.py
@@ -2,3 +2,4 @@ from straintables import primerFinder
 from straintables import matrixAnalysis, detectMutations, compareHeatmap, initializePrimerFile
 from straintables import logo
 from straintables import skdistance
+from straintables import InputFile
