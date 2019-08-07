2f6b296e429c9752d3be618e3d8545df8ed49f48
Gabriel Araujo <gabriel_scf@hotmail.com>
2019-08-07 12:15:45 -0300

    new file, moving methods for analysis input files here.

--- straintables/InputFile.py
+++ straintables/InputFile.py
@@ -13,22 +13,20 @@ def loadPrimerList(filePath):
 
     fileColumns = list(lociPrimerList.columns)
 
-    for i in range(len(fileColumns)):
-        if "Unnamed" in fileColumns[i]:
-            fileColumns[i] = np.nan
+    while len(fileColumns) < len(expectedColumns):
+        fileColumns.append(np.nan)
 
     if fileColumns != expectedColumns:
-        newFirstRowData = dict([(expected, fileColumns[e])
-                                for e, expected
-                                in enumerate(expectedColumns)])
+        lociPrimerList = pd.read_csv(filePath, header=None)
 
-        newFirstRow = pd.DataFrame([newFirstRowData],
-                                   columns=expectedColumns)
+        # -- Make sure data has the correct number of columns;
+        while lociPrimerList.shape[1] < len(expectedColumns):
+            lociPrimerList[str(lociPrimerList.shape[1])] =\
+                np.full(lociPrimerList.shape[0], np.nan)
+
+        if lociPrimerList.shape[1] > len(expectedColumns):
+            lociPrimerList = lociPrimerList.loc[:, :len(expectedColumns)-1]
 
         lociPrimerList.columns = expectedColumns
-
-        lociPrimerList = pd.concat([newFirstRow, lociPrimerList],
-                                   axis=0,
-                                   ignore_index=True).reset_index(drop=True)
 
     return lociPrimerList
