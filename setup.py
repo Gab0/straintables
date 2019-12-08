1ec9048eb76299eced307e4d5dc0e9c8b43ffb71
Gabriel Araujo <gabriel_scf@hotmail.com>
2019-12-08 15:21:50 -0300

    .

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ base_folder = os.path.dirname(os.path.realpath(__file__))
 requirements = list(open(os.path.join(base_folder, "requirements.txt")).readlines())
 setup(
     name='straintables',
-    version='0.996',
+    version='1.0',
     description='Genomic similarities per region',
     author='Gabriel Araujo',
     author_email='gabriel_scf@hotmail.com',
