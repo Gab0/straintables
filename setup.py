3b50790efe11af661ac30e10010cf998ff59feed
Gabriel Araujo <gabriel_scf@hotmail.com>
2020-06-01 12:43:48 -0300

    Viver: Export EPS files.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ requirements = list(
 
 setup(
     name='straintables',
-    version='1.42',
+    version='1.50',
     description='Build & Compare dissimilarity matrices for genomic regions',
     author='Gabriel Araujo',
     author_email='gabriel_scf@hotmail.com',
